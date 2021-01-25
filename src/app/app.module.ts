d8f7991c292457c06cdedc0ddcae2c5f90074ee5
Venczel Patrik <v.papatrik@gmail.com>
2021-01-25 01:37:16 +0100

    style: add angular material on buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharactersModule } from './characters/characters.module';
 import { HomeComponent } from './home/home.component';
+import { MatButtonModule } from '@angular/material/button'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     CharactersModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -4,15 +4,25 @@ import { CharacterDetailsComponent } from './character-details/character-details
 import { CharacterListComponent } from './character-list/character-list.component';
 import { CharacterRoutingModule } from './character-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button'
+import { SearchFormComponent } from './search-form/search-form.component';
+
 
 
 
 @NgModule({
-  declarations: [CharacterDetailsComponent, CharacterListComponent],
+  declarations: [CharacterDetailsComponent, CharacterListComponent, SearchFormComponent],
   imports: [
     CommonModule,
     CharacterRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   exports:[
     CharacterListComponent,
